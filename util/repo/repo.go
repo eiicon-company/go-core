36b3743683f4eaa55f4cf23ac8bd410df8685c47
ikeikeikeike <jp.ne.co.jp@gmail.com>
2020-05-21 18:42:32 +0900

    replace loader type string slice to qm.QueryMod Slice which there are for compatibility

--- util/repo/repo.go
+++ util/repo/repo.go
@@ -1,7 +1,6 @@
 package repo
 
 import (
-	"github.com/eiicon-company/go-core/util/priv"
 	"github.com/volatiletech/sqlboiler/queries/qm"
 	"golang.org/x/xerrors"
 )
@@ -9,17 +8,11 @@ import (
 const (
 	// DBFormat translates as common database datetime format
 	DBFormat = "2006-01-02 15:04:05"
-	maxUint  = ^uint(0)
-	maxInt   = int(maxUint >> 1)
-	// minUint  = 0
-	// minInt   = -maxInt - 1
 )
 
 var (
 	// ErrExists a record already exists
 	ErrExists = xerrors.New("already exists")
-	// preloadLimit prevent to get entire record
-	preloadLimit = 1000
 )
 
 var (
@@ -27,34 +20,24 @@ var (
 	AscOrder = qm.OrderBy("id ASC")
 	// DescOrder defines ORDER BY query as descending
 	DescOrder = qm.OrderBy("id DESC")
-	// Load preloads with preloadLimit
-	Load = func(relationship string, mods ...qm.QueryMod) qm.QueryMod {
-		return qm.Load(relationship, append(mods, qm.Limit(preloadLimit))...)
-	}
 )
 
-// SetPreloadLimit changes any number to preloadLimit
-func SetPreloadLimit(limit int) {
-	if preloadLimit < 0 {
-		preloadLimit = maxInt
-	} else {
-		preloadLimit = limit
-	}
-}
+// PreloadByID assembles QueryMod with primary ID
+//
+// XXX: There is this code for compatibility
+//
+func PreloadByID(id int, loads ...qm.QueryMod) []qm.QueryMod {
+	mods := []qm.QueryMod{qm.Where("id = ?", id)}
+	mods = append(mods, loads...)
 
-// Fuzzy this is so fuzzzy
-func Fuzzy(v interface{}, arr []interface{}) bool {
-	for _, i := range arr {
-		if priv.MustString(i) == priv.MustString(v) {
-			return true
-		}
-	}
-
-	return false
+	return mods
 }
 
 // PreloadBy assembles QueryMod with where statements
-func PreloadBy(where []qm.QueryMod, loads ...string) ([]qm.QueryMod, error) {
+//
+// XXX: There is this code for compatibility
+//
+func PreloadBy(where []qm.QueryMod, loads ...qm.QueryMod) ([]qm.QueryMod, error) {
 	if len(where) <= 0 {
 		return nil, xerrors.New("no queries")
 	}
@@ -62,22 +45,36 @@ func PreloadBy(where []qm.QueryMod, loads ...string) ([]qm.QueryMod, error) {
 	return append(where, Preloads(loads...)...), nil
 }
 
-// Preload assembles QueryMod with primary id
-func Preload(id int, loads ...string) []qm.QueryMod {
-	mods := []qm.QueryMod{qm.Where("id = ?", id)}
-	for _, load := range loads {
-		mods = append(mods, Load(load))
-	}
+// Preloads assembles loads
+//
+// XXX: There is this code for compatibility
+//
+func Preloads(loads ...qm.QueryMod) []qm.QueryMod {
+	mods := []qm.QueryMod{}
+	mods = append(mods, loads...)
 
 	return mods
 }
 
-// Preloads assembles loads
-func Preloads(loads ...string) []qm.QueryMod {
-	mods := []qm.QueryMod{AscOrder}
-	for _, load := range loads {
-		mods = append(mods, Load(load))
+// DescPreloadBy assembles QueryMod with where statements
+//
+// XXX: There is this code for compatibility
+//
+func DescPreloadBy(where []qm.QueryMod, loads ...qm.QueryMod) ([]qm.QueryMod, error) {
+	if len(where) <= 0 {
+		return nil, xerrors.New("no queries")
 	}
+
+	return append(where, DescPreloads(loads...)...), nil
+}
+
+// DescPreloads assembles loads
+//
+// XXX: There is this code for compatibility
+//
+func DescPreloads(loads ...qm.QueryMod) []qm.QueryMod {
+	mods := []qm.QueryMod{DescOrder}
+	mods = append(mods, loads...)
 
 	return mods
 }
