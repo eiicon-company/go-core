8c11a17f4ed4d2f00289887343944cd7f28395f3
ikedat / Tatsuo Ikeda <jp.ne.co.jp@gmail.com>
2023-06-04 18:10:40 +0900

    dlm adds exists & mutex option method (#34)
    * dlm-exists-method
    
    * added method's commend

--- util/dlm/dlm.go
+++ util/dlm/dlm.go
@@ -20,7 +20,31 @@ func (d *DLM) Mutex(name string, expires time.Duration) *redsync.Mutex {
 	return rs.NewMutex(name, redsync.SetExpiry(expires))
 }
 
+// MutexOptions returns MUTual EXclusion
+func (d *DLM) MutexOptions(name string, options ...redsync.Option) *redsync.Mutex {
+	rs := redsync.New([]redsync.Pool{d.Pool})
+	return rs.NewMutex(name, options...)
+}
+
 // Close dlm connection pooling
 func (d *DLM) Close() error {
-	return d.Pool.Close()
+	//
+	// return d.Pool.Close() // never close cause of one pool is used right now
+	//
+	return nil
+}
+
+// Exists checks a data is existence or not.
+func (d *DLM) Exists(name string) (bool, error) {
+	conn := d.Pool.Get()
+	defer conn.Close()
+
+	reply, err := redis.String(conn.Do("GET", name))
+	if err != nil && err == redis.ErrNil {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+	return reply != "", nil
 }
